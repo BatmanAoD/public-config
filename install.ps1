fbf55ea9e8b65135265b2aca909db0cd80c39b3d
Kyle Strand <kyle.strand@pieinsurance.com>
2021-10-17 17:18:32 -0600

    PS files

---
+++ install.ps1
@@ -0,0 +1,8 @@
+# use 'gsudo' if no admin permissions (how can we check?)
+
+# Installs: git, scoop (?), latest PowerShell
+
+# Needs elevated permissions
+set-service beep -startuptype disabled
+
+# TODO: everything else...
