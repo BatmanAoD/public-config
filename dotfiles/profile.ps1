f70d78dcb4b16f335dd22d3163891b148ac81a6e
Kyle Strand <kyle.strand@pieinsurance.com>
2023-04-04 10:20:33 -0600

    install git autocompletion for powershell

--- dotfiles/profile.ps1
+++ dotfiles/profile.ps1
@@ -2,3 +2,5 @@ Set-PSReadLineOption -EditMode vi
 Import-Alias ~\public-config\ps-aliases.csv
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+Import-Module posh-git
--- install.ps1
+++ install.ps1
@@ -9,7 +9,7 @@ if (-Not (Get-Command choco -errorAction SilentlyContinue)) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-choco install -y git pwsh gsudo neovide zulip microsoft-windows-terminal alacritty element-desktop greenshot firacode fzf
+choco install -y git pwsh gsudo neovide zulip microsoft-windows-terminal alacritty element-desktop greenshot firacode fzf poshgit
 # TODO remove '--pre' if/when package gets approved
 choco install -y --pre xmouse-controls --version 1.1.0.0
 
