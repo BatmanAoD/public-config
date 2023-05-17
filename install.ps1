927b012e633ccd577cc38807324ef0262c8d0daa
Kyle J Strand <batmanaod@gmail.com>
2023-05-16 23:50:17 -0600

    wezterm

--- install.ps1
+++ install.ps1
@@ -9,7 +9,7 @@ if (-Not (Get-Command choco -errorAction SilentlyContinue)) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-choco install -y git pwsh gsudo neovide zulip microsoft-windows-terminal alacritty element-desktop greenshot firacode fzf poshgit xmouse-controls
+choco install -y git pwsh gsudo neovide zulip microsoft-windows-terminal alacritty element-desktop greenshot firacode fzf poshgit xmouse-controls wezterm
 
 # TODO: accept prompts, but don't update unless necessary
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
